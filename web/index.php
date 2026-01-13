1bb3cd9320ceb3a6fe5c9dd48ef3fcb7e8f424f7
cubixo <cubixo2302@gmail.com>
2026-01-13 16:10:05 +0100

    création du header avec menu burger

--- web/index.php
+++ web/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- icon -->
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-    
+    <header>
+        <?php include 'php_parts/header.php' ?>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <?php include 'php_parts/footer.php' ?>
+    </footer>
 </body>
+
 </html>
---
+++ web/php_parts/header.php
@@ -0,0 +1,24 @@
+<script src="script/scriptHeader.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<nav>
+    <a href="index.php" class="logo"><img src="img/logo_aero.webp" alt="logo"></a>
+
+    <div class="nav-right">
+        <a href="#" class="btn-reserve">Réserver</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+    <div id="myLinks">
+        <a href="index.php">Accueil</a>
+        <a href="#">L'école</a>
+        <a href="#">Avions</a>
+        <a href="#">Services</a>
+        <a href="#">Actualités</a>
+        <div class="line"></div>
+        <a href="#">Espace Membres</a>
+        <a href="#">Contact</a>
+    </div>
+</nav>
