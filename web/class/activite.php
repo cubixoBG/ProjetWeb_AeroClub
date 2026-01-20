0e3fcb9e58af6af9b8563a56bcc140fdc211689c
CdP cubixoBG <cdp cubixo2302@gmail.com>
2026-01-20 20:04:50 +0100

    Ajout de la méthode getAll pour récupérer toutes les activités

--- web/class/activite.php
+++ web/class/activite.php
@@ -6,4 +6,10 @@ class Activite {
     public function __construct($db) {
         $this->db = $db;
     }
+
+    
+    public function getAll() {
+        $query = "SELECT * FROM Activite ORDER BY ordre ASC, id ASC";
+        return $this->db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
